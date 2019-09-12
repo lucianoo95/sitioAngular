714d5be2c4a9d0cebb0fc92617ddbf7e1afa6fcf
Luciano <lucianooo222@gmail.com>
2019-09-12 17:37:08 -0300

    Se agregaron componentes nuevos y se modifico el archivo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    EquiposComponent
   ],
   imports: [
     BrowserModule,
